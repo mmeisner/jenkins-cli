b6dd9473eb28f5715940503ccde3931d0b27eee0
Mads Meisner-Jensen <mmeisner@users.noreply.github.com>
2021-11-09 19:59:17 +0100

    vastly improve bash completion script
    Old script was pretty rubbishy

--- jenkins-completion.bash
+++ jenkins-completion.bash
@@ -2,12 +2,32 @@
 # Provide bash completions for job names on the (configured) Jenkins server
 # Source this script from your .bashrc or copy it into /etc/bash_completion.d/
 
-_jenkins_completions()
+_jenkins()
 {
-    [ "${#COMP_WORDS[@]}" == "2" ] || return
+    local cur prev opts
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    jobs=$(jenkins --list 2>/dev/null)
-    COMPREPLY=($(compgen -W "${jobs}" "${COMP_WORDS[1]}"))
+    # only include most commonly used options in completion
+    opts_short="-b -p -B -c -w -i -a -v -o -d"
+    opts_long="--groovy --get-groovy --get-config ---post-config --arti --list --que --nodes --makeconf"
+
+    COMPREPLY=()
+
+    if [[ ${cur} == -* ]] ; then
+        # if only one dash, expand all options
+        COMPREPLY=( $(compgen -W "${opts_short} ${opts_long}" -- ${cur}) )
+        return 0
+    elif [[ ${cur} == --* ]] ; then
+        # if two dashes, expand only long options
+        COMPREPLY=( $(compgen -W "${opts_long}" -- ${cur}) )
+        return 0
+    else
+        # else expand jobs
+        jobs=$(jenkins --list 2>/dev/null)
+        COMPREPLY=($(compgen -W "${jobs}" -- "${cur}"))
+        return 0
+    fi
 }
-complete -F _jenkins_completions jenkins
-complete -F _jenkins_completions jenkins.py
+complete -F _jenkins jenkins
+complete -F _jenkins jenkins.py
